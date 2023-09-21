7697682f300113f8f6578df7c40d856a5bed6c7d
Max Bezditko <mb.preply.1@gmail.com>
2023-09-21 13:50:06 +0300

    feat(auth): implement GraphQL inputs validation

--- src/models/inputs/login-with-email.input.ts
+++ src/models/inputs/login-with-email.input.ts
@@ -1,8 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail } from 'class-validator';
 
 @InputType()
 export class LoginWithEmailInput {
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
